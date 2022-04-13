b17401868674b285d59d2a37463afa0d38ca9ae5
Niclas Wilkin <niclas.wilkin94@web.de>
2022-04-13 14:21:54 +0200

    monitor suppression list of AWS SES

---
+++ bin/ses_suppression_check.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+POSITIONAL_ARGS=()
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -e|--email)
+      EMAIL="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+if [[ -z $EMAIL ]]; then
+    echo -e "Parameter missing! Usage: sh $0 '--email user@example.org'"
+else
+    aws sesv2 get-suppressed-destination --email-address $EMAIL > /dev/null 2>&1
+    if [[ $? == 0 ]]; then
+        echo "Failure - Email address ${EMAIL} exist on your suppression list."
+    aws sesv2 delete-suppressed-destination --email-address $EMAIL > /dev/null 2>&1
+    else
+        echo "OK - Email address ${EMAIL} does not exist on your suppression list."
+    fi
+fi
