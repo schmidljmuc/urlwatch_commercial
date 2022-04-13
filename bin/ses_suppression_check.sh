a68e1ca6f16e7c95865c6a2544326a93913c287e
Niclas Wilkin <niclas.wilkin94@web.de>
2022-04-13 09:30:46 +0200

    Add wrapper shell script for ses suppression check

---
+++ bin/ses_suppression_check.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+POSITIONAL_ARGS=()
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -e|--email)
+      EMAIL="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+if [[ -z $EMAIL ]]; then
+    echo -e "Parameter missing! Usage: sh $0 '--email user@example.org'"
+else
+    aws sesv2 get-suppressed-destination --email-address $EMAIL
+    if [[ $? == 1 ]]; then
+        echo "OK - Email address ${EMAIL} does not exist on your suppression list."
+    else
+        echo "Failure - Email address ${EMAIL} is on your suppression list."
+    fi
+fi
